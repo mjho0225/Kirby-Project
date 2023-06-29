d84a781a6da7f9b30fad4381175b5d72a8dd2690
mjho0225 <mjho0225@gamil.com>
2023-06-29 09:58:57 +0900

    맵 프리팹화 및 에너미 스크립트 추가
    1스테이지 테스트 맵 사용가능

---
+++ Assets/2. Scripts/1) JH_Scripts/JH_Enemy_Patrol.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// 패트롤을 네브메쉬를 사용하지 않고 구현
+// 1.   패트롤하고자 하는 위치를 저장한다.
+// 1-1. 리스트를 만들어 패트롤 하고자하는 위치를 받는다.
+// 2.   리스트에 있는 패트롤 위치로 이동한다.
+// 2-1. 리스트에 있는 패트롤 위치로 순차적으로 이동한다.
+// 3.   순차적으로 이동 후 마지막 지점에서는 다시 첫번째 리스트의 위치로 이동한다.
+// 4.   만약, 순차적으로 이동 중에 플레이어가 감지 되면 플레이어에게 이동한다.
+
+
+public class JH_Enemy_Patrol : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
