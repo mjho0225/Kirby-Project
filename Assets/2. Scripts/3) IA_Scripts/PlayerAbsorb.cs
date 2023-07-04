328a5505adb93d48d0fd96f8ff09154e78fe9495
DESKTOP-G4U9APQ\user <devcoderian@gmail.com>
2023-07-04 13:34:15 +0900

    player prefab trigger추가

--- Assets/2. Scripts/3) IA_Scripts/PlayerAbsorb.cs
+++ Assets/2. Scripts/3) IA_Scripts/PlayerAbsorb.cs
@@ -18,17 +18,20 @@ public class PlayerAbsorb : MonoBehaviour
 
     //int layer;
     float power = 10f;
-    GameObject absorbItem;
+    [SerializeField] public GameObject absorbItem;
     string absorbItemTag;
-    bool isAbsorb = false;
-    public bool isAbsorbing = false;
-    public GameObject absorbCollider;
+    public bool isAbsorb = false;
+    //public bool isAbsorbing = false;
+    GameObject absorbTrigger;
     // Start is called before the first frame update
     void Start()
     {
         print("흡수 동작");
         emptyState = true;
-     
+        absorbTrigger = GameObject.Find("AbsorbTrigger");
+       
+        //print("absorbTrigger" + absorbTrigger.GetComponent<SphereCollider>());
+        //OnAbsorbCollider();
 
         //gameObject.GetComponentInChildren<SphereCollider>().enabled = true;
     }
@@ -57,10 +60,12 @@ public class PlayerAbsorb : MonoBehaviour
                     item = hitInfo.collider.gameObject.transform.parent.gameObject;
                 if (hitInfo.collider.gameObject.layer == 6)
                     {
+                        OnAbsorbCollider();
                         UpdateGetItem(item);
                      }
                 }
-         }
+      
+        }
          
         if(Input.GetButtonDown("Fire1"))
         {
@@ -89,73 +94,66 @@ public class PlayerAbsorb : MonoBehaviour
                 emptyState = true;
                 absorbItemTag = null;
 
-            }
-            
+            }   
         }
+        if (Input.GetButtonUp("Fire1") && isAbsorb)
+        {
+            print("FIRE");
+       
+        }
+
 
     }
 
     void UpdateGetItem(GameObject item)
 
     {
+        isAbsorb = true;
+        absorbItem = item;
         print("Game" + item);
         //상태 체크
-        if (currTime > 2f)
+        if (currTime > 1.5f)
         {
             float distance = Vector3.Distance(item.transform.position, transform.position); //자석의 거리를 재는 코드.
 
             print("distance" + distance);
-             
-            //if (distance <= 10.0f)
-            //{
                 item.transform.LookAt(transform.position);
                 item.transform.Translate(Vector3.forward * speed * Time.deltaTime);
-                absorbItem = item;
-                isAbsorb = true;
-
-            //absorbCollider = GameObject.Find("AbsorbTrigger");
-            //absorbCollider.GetComponent<SphereCollider>().enabled = true;
-            //gameObject.GetComponent<SphereCollider>().enabled = false;
+              
         }
 
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnAbsorbCollider()
     {
- 
-        if (other.gameObject.layer == LayerMask.NameToLayer("Item"))
+        SphereCollider[] mesh_origin = GetComponents<SphereCollider>();
+        SphereCollider mesh_child = absorbTrigger.GetComponent<SphereCollider>();
+        for(int i = 0; i < mesh_origin.Length; i++)
         {
-            //아이템 분류 후 획득
-            if(other.gameObject.tag == "item")
-            {
-             
-            }
+            mesh_origin[i].enabled = false;
         }
+        mesh_child.enabled = true;
 
-        //print("other check" + other.gameObject);
-        //print("layer check" + layer);
-        // if (other.gameObject.layer == LayerMask.NameToLayer("Enemy"))//&& !emptyState
-        if (other.gameObject == absorbItem)
+    }
+    void OffAbsorbCollider()
+    {
+        SphereCollider[] mesh_origin = GetComponents<SphereCollider>();
+        SphereCollider mesh_child = absorbTrigger.GetComponent<SphereCollider>();
+        for (int i = 0; i < mesh_origin.Length; i++)
         {
-          
-            absorbItemTag = other.gameObject.tag;
-            
-            //게임 오브젝트 흡입 후 소멸
-            Destroy(other.gameObject);
-            currTime = 0;
-            emptyState = false;
-            
-            IsAttackItem(absorbItemTag, isAbsorb);
-
-
+            mesh_origin[i].enabled = true;
         }
+        mesh_child.enabled = false;
+
     }
     
     //attack 사용 상태 체크
     //itemIventory();
-    void IsAttackItem(string absorbItemTag, bool isAbsorb)
+    public void IsAttackItem(string absorbItemTag)
     {
-
+        currTime = 0;
+        emptyState = false;
+        OffAbsorbCollider();
         switch (absorbItemTag)
         {
             case "e_ranger":
@@ -163,7 +161,7 @@ public class PlayerAbsorb : MonoBehaviour
                 absorbItemTag = "MonsterPos";
                 if (isAbsorb)
                 {
-                    isAbsorbing = false;
+                    isAbsorb = false;
                     print("변신 애니메이션 + 시간 흐른 후 ranger로 변경 + 현재 총알 나가는 오류 있음");
                     GetComponent<PlayerController>().attackState = PlayerController.AttackState.RANGER;
                 }
@@ -171,7 +169,8 @@ public class PlayerAbsorb : MonoBehaviour
                 {
                     GetComponent<PlayerController>().attackState = PlayerController.AttackState.ABSORB;
                 }
-              
+                //매번해주기
+                OffAbsorbCollider();
                 break;
 
             default:
@@ -192,3 +191,33 @@ public class PlayerAbsorb : MonoBehaviour
     //}
 
 }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+ 
+    //    if (other.gameObject.layer == LayerMask.NameToLayer("Item"))
+    //    {
+    //        //아이템 분류 후 획득
+    //        if(other.gameObject.tag == "item")
+    //        {
+             
+    //        }
+    //    }
+
+    //    //print("other check" + other.gameObject);
+    //    //print("layer check" + layer);
+
+    //    if (other.gameObject == absorbItem)
+    //    {
+          
+    //        absorbItemTag = other.gameObject.tag;
+            
+    //        //게임 오브젝트 흡입 후 소멸
+    //        Destroy(other.gameObject);
+    //        currTime = 0;
+    //        emptyState = false;
+            
+    //        IsAttackItem(absorbItemTag, isAbsorb);
+
+    //    }
+    //}
