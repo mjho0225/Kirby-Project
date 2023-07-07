2dc8c4365bf100fb500087a25ad8a2d884c50a57
mjho0225 <mjho0225@gamil.com>
2023-07-07 18:31:41 +0900

    자동차 레이어 커밋

--- Assets/2. Scripts/1) JH_Scripts/JH_SpawnStart.cs
+++ Assets/2. Scripts/1) JH_Scripts/JH_SpawnStart.cs
@@ -37,7 +37,7 @@ public class JH_SpawnStart : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" || other.gameObject.layer == LayerMask.NameToLayer("Car"))
         {
             if (this.gameObject.name.Contains("BigRock"))
             {
