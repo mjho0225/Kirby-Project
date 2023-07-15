24c55905259064e65d00c34a7a7155d0e12f7dc6
devCoderian <87194565+devCoderian@users.noreply.github.com>
2023-07-15 23:00:44 +0900

    bullet01 effect 처리
    에너미나 적에 부딪혔을때 이펙트, destroy

--- Assets/2. Scripts/3) IA_Scripts/Bullet.cs
+++ Assets/2. Scripts/3) IA_Scripts/Bullet.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int damage;
+    public GameObject bulletStar;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +21,17 @@ public class Bullet : MonoBehaviour
     //벽과 Ground 설정하기
     private void OnCollisionEnter(Collision collision)
     {
+
         //파티클
         // GameObject bullet02 = Instantiate(bulletFactory02, firePos02.point, Quaternion.LookRotation(firePos02.normal));
         //Destroy(gameObject, 4);
-        //if (collision.gameObject.tag == "Ground" || collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-        //{
-        //    Destroy(gameObject);
-        //}
-        Destroy(gameObject);
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+
+            Instantiate(bulletStar, collision.contacts[0].point, Quaternion.identity);
+            
+            Destroy(gameObject);
+        }
+       
     }
 }
