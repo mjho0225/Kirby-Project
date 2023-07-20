c5b4987d86c83c777b70765165d39b496f135b9e
xalswox <qoalswo123@icloud.com>
2023-07-20 12:46:56 +0900

    Fix: Camera 변신할 때만 Layer변경
    변신: 카메라 레이아웃 끄기, 변신 후: 카메라 레이아웃 원래 상태

--- Assets/2. Scripts/2) MJ_Scripts/CameraLayerManager.cs
+++ Assets/2. Scripts/2) MJ_Scripts/CameraLayerManager.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//메인 카메라의 culling Mask를 vfx, car, player 빼고 다 실행시키고 싶다.
-// 백그라운드 카메라 vfx, car, player를 키고 나머지를 끄고싶다.
+// 메인 카메라의 culling mask를 제어하고 싶다.
+// 하위 카메라의 culling mask를 제어하고 싶다.
 public class CameraLayerManager : MonoBehaviour
 {
     // 카메라의 객체를 가져온다.
@@ -11,19 +11,29 @@ public class CameraLayerManager : MonoBehaviour
 
     private int VFX_Car_Player_MaskNumber;
 
-    void Start()
+    void Awake()
     {
         VFX_Car_Player_MaskNumber = ((1 << LayerMask.NameToLayer("VFX") | (1 << LayerMask.NameToLayer("Car")) | (1 << LayerMask.NameToLayer("Player"))));
     }
-    public void OnMainCamera_VFX_Car_Player_Layer()
+
+
+    public void ChangedOffCameraLayer()
     {
-        // 레이어마스크 이름을 기준으로 제외하고 모든 것을 키고싶다.
-        Camera.main.cullingMask = ~VFX_Car_Player_MaskNumber;
+        //메인 카메라 레이아웃을 다 키고싶다.
+        Camera.main.cullingMask = -1;
+        // 하위 카메라 레이아웃을 다 끄고 싶다.
+        backGroundCamera.cullingMask = ~-1;
+
     }
 
-    public void OnBackgroundCamera_VFX_Car_Player_Layer()
+
+    public void ChangingOnCameraLayer()
     {
-        // 레이어마스크 이름을 선택한 것을 모두 키고싶다.
+        // 메인 카메라의 다 키고 vfx, player, car 3개만 끄고싶다
+        Camera.main.cullingMask = ~VFX_Car_Player_MaskNumber;
+        // 하위 카메라의 다 끄고 vfx, player, car 3개만 키고싶다.
         backGroundCamera.cullingMask = VFX_Car_Player_MaskNumber;
+
     }
+
 }
