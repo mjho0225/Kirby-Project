f01ee72df4924a95c8d400f8c79f7ccc4062a507
xalswox <qoalswo123@icloud.com>
2023-07-21 18:42:14 +0900

    Fix: 커비 변신시 카메라 중앙에 위치

--- Assets/2. Scripts/2) MJ_Scripts/CameraLayerManager.cs
+++ Assets/2. Scripts/2) MJ_Scripts/CameraLayerManager.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 // 메인 카메라의 culling mask를 제어하고 싶다.
 // 하위 카메라의 culling mask를 제어하고 싶다.
+// 플레이어를 카메라 중앙에 위치시키고 싶다.
+// 카메라의 뷰포트 값을 월드 위치 값으로 변경한다.
+// 플레이어의 위치 값에 카메라의 위치 값으로 변경한다.
 public class CameraLayerManager : MonoBehaviour
 {
     // 카메라의 객체를 가져온다.
     public Camera backGroundCamera;
+    public GameObject player;
 
     private int VFX_Car_Player_MaskNumber;
 
@@ -33,6 +37,16 @@ public class CameraLayerManager : MonoBehaviour
         Camera.main.cullingMask = ~VFX_Car_Player_MaskNumber;
         // 하위 카메라의 다 끄고 vfx, player, car 3개만 키고싶다.
         backGroundCamera.cullingMask = VFX_Car_Player_MaskNumber;
+
+        //카메라의 중앙값
+        Vector3 cameraViewCenterPosition = new Vector3(0.5f, 0.5f, 0);
+
+        // 카메라의 뷰포트 값을 좌표 값으로 만든다..
+        Vector3 cameraWorldPosition = Camera.main.ViewportToWorldPoint(cameraViewCenterPosition);
+
+
+        // 플레이어 위치는 카메라 좌우의 중앙에 있고, 플레이어는 자기 자신의 y,z 축에 있다.
+        player.transform.position = new Vector3(cameraWorldPosition.x, player.transform.position.y, player.transform.position.z);
 
     }
 
