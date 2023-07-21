05a6eebce4b200117692c857ff72384b7763eb72
xalswox <qoalswo123@icloud.com>
2023-07-21 15:28:05 +0900

    Fix: 리스폰 버그 & 카메라 시야에서 물체 뚫리는 오류 수정
    특정 위치 이하로 갈때 리스폰, Camera Near Flip Plane 값 1로 변경

--- Assets/2. Scripts/2) MJ_Scripts/DeathWall.cs
+++ Assets/2. Scripts/2) MJ_Scripts/DeathWall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using EZCameraShake;
 
-// 플레이어가 밑에 하단에서 레이 캐스트를 쏳았을 때 지면에 닿았다면 발생시킨다.
+// 플레이어가 일정 위치에서 떨어지면 다시 태어나게 만든다. ( 10 위치에서 )
 public class DeathWall : MonoBehaviour
 {
     // 플레이어를 가져온다.
@@ -11,9 +11,22 @@ public class DeathWall : MonoBehaviour
     // 체크 포인트를 가져온다.
     public GameObject checkPoint;
 
+    // 플레이어 최대 떨어지는 거리
+    public float maxFallPosition = 10;
+
+    void Update()
+    {
+        // 플레이어가 일정 위치에서 떨어지면 다시 태어나게 만든다. ( 10 위치에서 )
+        if (player.transform.position.y < maxFallPosition)
+        {
+            player.transform.position = checkPoint.transform.position;
+            // 카메라를 한번 쉐이킹 한다.
+            StartCoroutine(IEOnDamage());
+        }
+    }
 
     // 만약 플레이어가 태그 된다면 원래 위치에서 태어나게 한다.
-    private void OnTriggerEnter(Collider other)
+/*    private void OnTriggerEnter(Collider other)
     {
         
         if (other.gameObject.layer == LayerMask.NameToLayer("Car"))
@@ -23,7 +36,7 @@ public class DeathWall : MonoBehaviour
             StartCoroutine(IEOnDamage());
 
         }
-    }
+    }*/
 
     // 카메라 쉐이킹을 0.5초 후에 실행시키고 싶다.
     IEnumerator IEOnDamage()
